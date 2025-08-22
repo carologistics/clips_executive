5cb8264c743d107627862912bc2786474f2cf726
TarikViehmann <TarikViehmann@users.noreply.github.com>
2025-08-22 13:00:36 +0000

    deploy: 48459c30b797ed8f7411f688e43336a5c470351a

--- cx_protobuf_plugin/generated/doxygen/cx_protobuf_plugin.tag
+++ cx_protobuf_plugin/generated/doxygen/cx_protobuf_plugin.tag
@@ -7,21 +7,21 @@
       <type>boost::signals2::signal&lt; void(protobuf_comm::ProtobufStreamServer::ClientID, std::shared_ptr&lt; google::protobuf::Message &gt;)&gt; &amp;</type>
       <name>signal_server_sent</name>
       <anchorfile>classprotobuf__clips_1_1ClipsProtobufCommunicator.html</anchorfile>
-      <anchor>ad9b1d71d6433c0f0f6fff5f556631d43</anchor>
+      <anchor>a63ddae947df78b3e1f788f956713e3e0</anchor>
       <arglist>()</arglist>
     </member>
     <member kind="function">
-      <type>boost::signals2::signal&lt; void(std::string, unsigned short, std::shared_ptr&lt; google::protobuf::Message &gt;)&gt; &amp;</type>
+      <type>boost::signals2::signal&lt; void(std::string, uint16_t, std::shared_ptr&lt; google::protobuf::Message &gt;)&gt; &amp;</type>
       <name>signal_client_sent</name>
       <anchorfile>classprotobuf__clips_1_1ClipsProtobufCommunicator.html</anchorfile>
-      <anchor>af0cd4f4f1d5af8495c0dd955f8c97170</anchor>
+      <anchor>a25d865044f1659d7b2e01e76415ebc0f</anchor>
       <arglist>()</arglist>
     </member>
     <member kind="function">
-      <type>boost::signals2::signal&lt; void(long, std::shared_ptr&lt; google::protobuf::Message &gt;)&gt; &amp;</type>
+      <type>boost::signals2::signal&lt; void(int64_t, std::shared_ptr&lt; google::protobuf::Message &gt;)&gt; &amp;</type>
       <name>signal_peer_sent</name>
       <anchorfile>classprotobuf__clips_1_1ClipsProtobufCommunicator.html</anchorfile>
-      <anchor>aabee4aa597a5bc5f612c939c9267afea</anchor>
+      <anchor>a92030039e325ddd9cbb8168c28701c48</anchor>
       <arglist>()</arglist>
     </member>
   </compound>
--- cx_ros_param_plugin/generated/doxygen/html/search/functions_6.js
+++ cx_ros_param_plugin/generated/doxygen/html/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['signal_5fclient_5fsent_0',['signal_client_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#af0cd4f4f1d5af8495c0dd955f8c97170',1,'protobuf_clips::ClipsProtobufCommunicator']]],
-  ['signal_5fpeer_5fsent_1',['signal_peer_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#aabee4aa597a5bc5f612c939c9267afea',1,'protobuf_clips::ClipsProtobufCommunicator']]],
-  ['signal_5fserver_5fsent_2',['signal_server_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#ad9b1d71d6433c0f0f6fff5f556631d43',1,'protobuf_clips::ClipsProtobufCommunicator']]]
+  ['signal_5fclient_5fsent_0',['signal_client_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#a25d865044f1659d7b2e01e76415ebc0f',1,'protobuf_clips::ClipsProtobufCommunicator']]],
+  ['signal_5fpeer_5fsent_1',['signal_peer_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#a92030039e325ddd9cbb8168c28701c48',1,'protobuf_clips::ClipsProtobufCommunicator']]],
+  ['signal_5fserver_5fsent_2',['signal_server_sent',['http://docs.ros.org/en/latest/p/cx_protobuf_plugin/generated/doxygen/html/classprotobuf__clips_1_1ClipsProtobufCommunicator.html#a63ddae947df78b3e1f788f956713e3e0',1,'protobuf_clips::ClipsProtobufCommunicator']]]
 ];
