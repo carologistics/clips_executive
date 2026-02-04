359576f5759f811c5ea1083bd1b8e49b9b06a3e0
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2026-02-04 12:55:52 +0100

    cx_rl_bringup: rewrite launch to use substitutions

--- extensions/reinforment_learning/cx_rl_bringup/launch/agent.launch.py
+++ extensions/reinforment_learning/cx_rl_bringup/launch/agent.launch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env/python3
 # Copyright (c) 2026 Carologistics
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,27 +13,61 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#!/usr/bin/env python3
+# Copyright (c) 2026 Carologistics
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# See LICENSE file for details.
 
-import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
 
-def launch_with_context(context, *args, **kwargs):
-    cx_rl_bringup_dir = get_package_share_directory('cx_rl_bringup')
-    cx_bringup_dir = get_package_share_directory('cx_bringup')
+def generate_launch_description():
+    # -----------------------------
+    # Launch arguments
+    # -----------------------------
+    declare_log_level = DeclareLaunchArgument(
+        'log_level',
+        default_value='info',
+        description='Logging level for cx_node executable',
+    )
+
+    declare_manager_config = DeclareLaunchArgument(
+        'manager_config',
+        default_value='training_agent.yaml',
+        description='Name of the CLIPS environment manager configuration',
+    )
+
+    declare_training_config = DeclareLaunchArgument(
+        'training_config',
+        default_value='training_config.yaml',
+        description='Name of the RL training configuration',
+    )
+
+    # LaunchConfigurations
+    log_level = LaunchConfiguration('log_level')
     manager_config = LaunchConfiguration('manager_config')
     training_config = LaunchConfiguration('training_config')
-    rl_config = os.path.join(cx_rl_bringup_dir, 'params', training_config.perform(context))
-    log_level = LaunchConfiguration('log_level')
-    # also launch the pddl_manager
-    launch_cx = os.path.join(cx_bringup_dir, 'launch', 'cx_launch.py')
 
+    # -----------------------------
+    # Paths to packages and files
+    # -----------------------------
+    cx_rl_bringup_dir = get_package_share_directory('cx_rl_bringup')
+    cx_bringup_dir = get_package_share_directory('cx_bringup')
+
+    rl_config_file = PathJoinSubstitution([cx_rl_bringup_dir, 'params', training_config])
+    cx_launch_file = PathJoinSubstitution([cx_bringup_dir, 'launch', 'cx_launch.py'])
+
+    # -----------------------------
+    # Nodes and included launches
+    # -----------------------------
     cx_rl_node = Node(
         package='cx_rl_multi_robot_mppo',
         executable='cx_rl_node',
@@ -40,47 +75,26 @@ def launch_with_context(context, *args, **kwargs):
         name='cx_rl_bringup_rl_node',
         output='screen',
         emulate_tty=True,
-        parameters=[rl_config],
+        parameters=[rl_config_file],
     )
 
-    return [
-        cx_rl_node,
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(launch_cx),
-            launch_arguments={
-                'package': 'cx_rl_bringup',
-                'manager_config': manager_config.perform(context),
-                'log_level': log_level,
-            }.items(),
-        ),
-    ]
-
-
-def generate_launch_description():
-
-    LaunchConfiguration('model_file')
-
-    declare_log_level_ = DeclareLaunchArgument(
-        'log_level',
-        default_value='info',
-        description='Logging level for cx_node executable',
-    )
-    declare_manager_config = DeclareLaunchArgument(
-        'manager_config',
-        default_value='training_agent.yaml',
-        description='Name of the CLIPS environment manager configuration',
-    )
-    declare_training_config = DeclareLaunchArgument(
-        'training_config',
-        default_value='training_config.yaml',
-        description='Name of the RL training configuration',
+    include_cx_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(cx_launch_file),
+        launch_arguments={
+            'package': 'cx_rl_bringup',
+            'manager_config': manager_config,
+            'log_level': log_level,
+        }.items(),
     )
 
+    # -----------------------------
+    # Build LaunchDescription
+    # -----------------------------
     ld = LaunchDescription()
-
-    ld.add_action(declare_log_level_)
+    ld.add_action(declare_log_level)
     ld.add_action(declare_manager_config)
     ld.add_action(declare_training_config)
-    ld.add_action(OpaqueFunction(function=launch_with_context))
+    ld.add_action(cx_rl_node)
+    ld.add_action(include_cx_launch)
 
     return ld
