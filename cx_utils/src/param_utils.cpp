03871286fedd3fd499d849ccc6b8060ca45fa763
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2026-05-02 00:55:42 +0200

    cx_utils: improve resolve_files error cases
    Replace exit calls by throwing exceptions and also prevent crashes when
    packages cannot be found in the index. Instead, throw only if the result
    is that no matching file can be found.

--- cx_utils/src/param_utils.cpp
+++ cx_utils/src/param_utils.cpp
@@ -39,14 +39,21 @@ void resolve_files(
     } else {
       // Otherwise check in the respective share directories
       for (const auto & package : share_dirs) {
-        std::string dir_path = ament_index_cpp::get_package_share_directory(package);
-        std::filesystem::path share_dir_path(dir_path);
-
-        std::filesystem::path potential_path = share_dir_path / file_path;
-        if (std::filesystem::exists(potential_path)) {
-          files_out.push_back(potential_path);
-          found = true;
-          break;
+        try {
+          std::string dir_path = ament_index_cpp::get_package_share_directory(package);
+          std::filesystem::path share_dir_path(dir_path);
+          std::filesystem::path potential_path = share_dir_path / file_path;
+          if (std::filesystem::exists(potential_path)) {
+            files_out.push_back(potential_path);
+            found = true;
+            break;
+          }
+        } catch (const std::out_of_range & e) {
+          RCLCPP_WARN(
+            rclcpp::get_logger("cx_utils"),
+            "Share directory lookup skipped: package '%s' not found in ament index",
+            package.c_str());
+          continue;
         }
       }
     }
